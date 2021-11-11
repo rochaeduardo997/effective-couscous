dd371235da651123f11a4c83f6e61561930e13ea
rochaeduardo997 <rochaeduardo997@protonmail.com>
2021-11-10 21:03:01 -0300

    Created update route for availability

--- server/src/versions/v1/availability-ctrl.js
+++ server/src/versions/v1/availability-ctrl.js
@@ -18,6 +18,17 @@ module.exports = (app) => {
     start_in: Joi.number().min(1).required(),
     end_in:   Joi.number().max(86400).required(),
   });
+  const availabilityUpdateValidation = Joi.object({
+    availabilityId: Joi.string().guid({ version: [ 'uuidv4' ]}),
+    dayId:          Joi.string().guid({ version: [ 'uuidv4' ]}),
+    rangeId:        Joi.string().guid({ version: [ 'uuidv4' ]}),
+
+    day_name: Joi.string().valid('mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'),
+    status:   Joi.boolean(),
+
+    start_in: Joi.number().min(1),
+    end_in:   Joi.number().max(86400),
+  });
 
   async function index(req, res){
     try{
@@ -52,6 +63,41 @@ module.exports = (app) => {
       app.utils.printLog(printLogTitle, 'Route: Index', `Found: ${ availabilityResult.length }`, 'Status: true');
 
       return res.status(200).json({ status: true, found: availabilityResult.length, result });
+    }catch(err){
+      app.utils.printLog(printLogTitle, 'Route: Index', 'Status: false', err.message);
+
+      return res.status(500).json({ status: false, message: err.message });
+    }
+  }
+  async function findById(req, res){
+    const { id } = req.params;
+
+    try{
+      const availabilityResult = await app.Availability.findByPk(id);
+
+      let availabilityObj = {};
+      availabilityObj.id  = availabilityResult.id;
+
+      for(let day in availabilityResult.dataValues){
+        if(day !== 'id') {
+          availabilityObj[day] = await app.Ranges.findOne({ 
+            where: { 
+              fk_days: availabilityResult.dataValues[ day ]
+            },
+            include:[{ 
+              model: app.Days,
+              attributes: { exclude: [ 'fk_table_locations', 'createdAt', 'updatedAt' ]}
+            }],
+            attributes: { 
+              exclude: [ 'fk_days' ]
+            }
+          });
+        }
+      }
+
+      app.utils.printLog(printLogTitle, 'Route: Index', 'Status: true');
+
+      return res.status(200).json({ status: true, found: availabilityResult.length, availabilityObj });
     }catch(err){
       app.utils.printLog(printLogTitle, 'Route: Index', 'Status: false', err.message);
 
@@ -114,7 +160,7 @@ module.exports = (app) => {
       await transaction.commit();
       app.utils.printLog(printLogTitle, 'Route: Create', `ID: ${ availabilityId }`, 'Status: true');
 
-      const result = await app.Availability.findByPk(availabilityId)
+      const result = await app.Availability.findByPk(availabilityId);
 
       return res.status(201).json({ status: true, result });
     }catch(err){
@@ -135,40 +181,62 @@ module.exports = (app) => {
     }
   }
 
-  // async function update(req, res){
-  //   const { id }           = req.params;
-  //   let   { availability } = req.body;
+  async function update(req, res){
+    const { id }           = req.params;
+    let   { availability } = req.body;
 
-  //   let transaction = await app.sequelize.transaction();
+    let transaction = await app.sequelize.transaction();
 
-  //   try{
-  //     await userUpdateValidation.validateAsync(req.body);
+    try{
+      await availabilityUpdateValidation.validateAsync({ availabilityId: id }); // first verification, availability only
+      
+      for(let fk_day in availability){
+         if(fk_day !== 'id'){
+          const { id: rangeId, start_in, end_in, Day } = availability[fk_day];
+          const { id: dayId, day_name, status }        = Day;
 
-  //     await app.Users.update({ first_name, surname, username, contact, email, password, profile, active }, { where: { id }}, { transaction });
+          if(start_in > end_in) throw new Error('Start cannot be greater than end');
 
-  //     await transaction.commit();
+          await availabilityUpdateValidation.validateAsync({ dayId, rangeId, day_name, status, start_in, end_in}); // second verification, ranges and day
 
-  //     app.utils.printLog(printLogTitle, 'Route: Update', 'Status: true');
+          const verifyIfRangeExist = await app.Ranges.findByPk(rangeId);
+          if(!verifyIfRangeExist) throw new Error('Range not exist');
 
-  //     return res.status(200).json({ status: true });
-  //   }catch(err){
-  //     await transaction.rollback();
+          const verifyIfDayExistInRanges = await app.Ranges.findOne({ where: { fk_days: dayId }});
+          if(!verifyIfDayExistInRanges) throw new Error('Day not exist');
 
-  //     if(err.errors){
-  //       if(err.errors[0].message){
-  //         app.utils.printLog(printLogTitle, 'Route: Update', 'Status: false', err.errors[0].message);
+          const verifyIfDayExist = await app.Days.findByPk(dayId);
+          if(!verifyIfDayExist) throw new Error('Day not exist');
 
-  //         return res.status(500).json({ status: false, message: err.errors[0].message });
-  //       }
-  //     }
-  //     app.utils.printLog(printLogTitle, 'Route: Update', 'Status: false', err.message);
+          await app.Ranges.update({ start_in, end_in }, { where: { id: rangeId }}, { transaction });
+          await app.Days.update({ status },             { where: { id: dayId }},   { transaction });
+        }
+      }
 
-  //     return res.status(500).json({ status: false, message: err.message });
-  //   }
-  // }
+      await transaction.commit();
 
-  app.get('/availability',     basicAuth, index);
+      app.utils.printLog(printLogTitle, 'Route: Update', 'Status: true');
+
+      return res.status(200).json({ status: true });
+    }catch(err){
+      await transaction.rollback();
+
+      if(err.errors){
+        if(err.errors[0].message){
+          app.utils.printLog(printLogTitle, 'Route: Update', 'Status: false', err.errors[0].message);
+
+          return res.status(500).json({ status: false, message: err.errors[0].message });
+        }
+      }
+      app.utils.printLog(printLogTitle, 'Route: Update', 'Status: false', err.message);
+
+      return res.status(500).json({ status: false, message: err.message });
+    }
+  }
+
+  app.get('/availability',     adminAuth, index);
+  app.get('/availability/:id', adminAuth, findById);
   app.post('/availability',    adminAuth, create);
-  // app.put('/availability/:id', adminAuth, update);
+  app.put('/availability/:id', adminAuth, update);
   // app.delete('/table/:id', adminAuth, remove);
 }
